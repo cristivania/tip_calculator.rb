f31b952a8f38e7270458b8ad5071fbfd005c4771
Cristina Rosario <crosariobatres@yahoo.com>
2017-06-13 18:33:13 -0400

    calculator

---
+++ tip_calculator.rb
@@ -0,0 +1,10 @@
+bill= ""
+tip_percent = 0.20
+
+puts "How much is the bill?"
+bill = gets.to_f
+puts "The bill is $#{'%.2f' % bill}"
+tip = bill * tip_percent
+puts "The tip is $#{'%.2f' % tip}"
+total = bill + tip
+puts "The total is $#{'%.2f' % total}"
